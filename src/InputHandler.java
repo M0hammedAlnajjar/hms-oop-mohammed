fd939b53f545ee915cd78a8e63960a64a4d16c2d
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-19 20:59:48 +0400

    feat(utils): add text input method [Task 2.6]

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -4,4 +4,11 @@ import java.util.Scanner;
 public class InputHandler {
 
     private Scanner scanner = new Scanner(System.in);
+
+    // Read text from the user
+    public String readText(String message) {
+
+        System.out.print(message);
+        return scanner.nextLine();
+    }
 }
