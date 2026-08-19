648615e47bfa131bcf106fc3005a2234c9439cfc
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-19 20:10:51 +0400

    feat(services): implement Manageable + Searchable in PatientService [Task 2.4]

--- src/services/PatientService.java
+++ src/services/PatientService.java
@@ -3,8 +3,9 @@ package services;
 
 import entities.Patient;
 import interfaces.Manageable;
+import interfaces.Searchable;
 
-public class PatientService implements Manageable {
+public class PatientService implements Manageable, Searchable {
 
 
     // Find a patient by ID
@@ -14,6 +15,26 @@ public class PatientService implements Manageable {
 
     // Find a patient by first name and last name
     public Patient findPatient(String firstName, String lastName) {
+        return null;
+    }
+    @Override
+    public void add(Object entity) {
+
+    }
+    @Override
+    public void removeById(String id) {
+
+    }
+    @Override
+    public Object[] getAll() {
+        return null;
+    }
+    @Override
+    public Object[] search(String keyword) {
+        return null;
+    }
+    @Override
+    public Object searchById(String id) {
         return null;
     }
 
