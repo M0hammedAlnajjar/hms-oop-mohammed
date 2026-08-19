9ada9e8cf154d08f41f9ef6b35d045cbbc53ffa4
mohammed <mooodo150@gmail.com>
2026-08-19 11:48:58 +0400

    feat(entities): add Doctor fields [Task 1.3]

--- src/entities/Doctor.java
+++ src/entities/Doctor.java
@@ -1,4 +1,11 @@
 package entities;
 
 public class Doctor extends Person {
+
+    private String specialization;
+    private int experienceYears;
+    private double consultationFee;
+    private String[] availableSlots;
+    private String[] assignedPatientIds;
+    private boolean onCall;
 }
