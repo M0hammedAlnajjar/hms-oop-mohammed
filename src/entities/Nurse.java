87fdea8c33d4af61950c030c65597aa3e625901f
mohammed <mooodo150@gmail.com>
2026-08-19 14:38:55 +0400

    feat(entities): I use constructor calling super first [Task 1:4]

--- src/entities/Nurse.java
+++ src/entities/Nurse.java
@@ -6,4 +6,27 @@ public class Nurse extends Person{
     private String[] assignedPatientIds;
     private int yearsOfService;
 
+
+    public Nurse( String id,
+                  String firstName,
+                  String lastName,
+                  String dateOfBirth,
+                  String gender,
+                  String phoneNumber,
+                  String email,
+                  String address,
+                  String nationalId,
+                  int age,
+                  boolean active,
+                  String departmentId,
+                  String shift,
+                  String[] assignedPatientIds,
+                  int yearsOfService) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, nationalId, age, active);
+        this.departmentId = departmentId;
+        this.shift = shift;
+        this.assignedPatientIds = assignedPatientIds;
+        this.yearsOfService = yearsOfService;
+
+    }
 }
