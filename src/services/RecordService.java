7fd6ca721b295323171fc74a249b6924a76f1fba
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-19 23:23:14 +0400

    feat(services): implement medical record removal by ID [Task 2.7]

--- src/services/RecordService.java
+++ src/services/RecordService.java
@@ -16,6 +16,20 @@ public class RecordService implements Manageable, Searchable {
     @Override
     public void removeById(String id) {
 
+        for (int i = 0; i < recordCount; i++) {
+
+            if (records[i].getRecordId().equals(id)) {
+
+                for (int j = i; j < recordCount - 1; j++) {
+                    records[j] = records[j + 1];
+                }
+
+                records[recordCount - 1] = null;
+                recordCount--;
+
+                return;
+            }
+        }
     }
 
     @Override
