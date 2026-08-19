f1b99896a6da30612d63019b0b67b1cbf3e16e75
mohammed <mooodo150@gmail.com>
2026-08-19 09:53:16 +0400

    feat(entities): add Patient fields [Task 1.2]

--- src/entities/Patient.java
+++ src/entities/Patient.java
@@ -1,6 +1,13 @@
 package entities;
 
 public class Patient extends Person{
+    private String bloodGroup;
+    private String emergencyContact;
+    private String registrationDate;
+    private String[] allergies;
+    private String[] medicalRecordIds;
+    private double outstandingBalance;
+    private boolean insured;
 
 
 }
