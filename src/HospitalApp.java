30c9291b7c65e19a61d7693ef1cc4b48c5b472e3
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-19 19:51:20 +0400

    feat(poly): HospitalApp polymorphism methods [Task 2.3]

--- src/HospitalApp.java
+++ src/HospitalApp.java
@@ -1,2 +1,52 @@
+import entities.Person;
+
 public class HospitalApp {
+
+    // Print all person information
+    public static void printAll(Person[] people) {
+
+        for (Person person : people) {
+            person.displayInfo();
+        }
+    }
+
+    // Count persons by type
+    public static int countByType(
+            Person[] people,
+            String type
+    ) {
+
+        int count = 0;
+
+        for (Person person : people) {
+
+            if (person.getClass()
+                    .getSimpleName()
+                    .equalsIgnoreCase(type)) {
+
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    // Find the oldest person
+    public static Person findOldest(Person[] people) {
+
+        if (people == null || people.length == 0) {
+            return null;
+        }
+
+        Person oldest = people[0];
+
+        for (Person person : people) {
+
+            if (person.getAge() > oldest.getAge()) {
+                oldest = person;
+            }
+        }
+
+        return oldest;
+    }
 }
