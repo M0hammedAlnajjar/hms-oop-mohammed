1c2d52d00d566313b2111184fc2fd5190a2b5641
mohammed <mooodo150@gmail.com>
2026-08-19 11:54:13 +0400

    feat(entities): add Doctor constructor with super [Task 1.3]

--- src/entities/Doctor.java
+++ src/entities/Doctor.java
@@ -8,4 +8,14 @@ public class Doctor extends Person {
     private String[] availableSlots;
     private String[] assignedPatientIds;
     private boolean onCall;
+
+    public Doctor(String id, String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, String nationalId, int age, boolean active, String[] assignedPatientIds, String[] availableSlots, double consultationFee, int experienceYears, boolean onCall, String specialization) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, nationalId, age, active);
+        this.assignedPatientIds = assignedPatientIds;
+        this.availableSlots = availableSlots;
+        this.consultationFee = consultationFee;
+        this.experienceYears = experienceYears;
+        this.onCall = onCall;
+        this.specialization = specialization;
+    }
 }
