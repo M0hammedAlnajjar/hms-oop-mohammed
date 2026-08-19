e66605183404eb9b73b4b52c3e59c59e717e070e
mohammed <mooodo150@gmail.com>
2026-08-19 10:24:57 +0400

    feat(entities): add Patient constructor with super [Task 1.2]

--- src/entities/Patient.java
+++ src/entities/Patient.java
@@ -9,5 +9,14 @@ public class Patient extends Person{
     private double outstandingBalance;
     private boolean insured;
 
-
+    public Patient(String id, String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, String nationalId, int age, boolean active, String[] allergies, String bloodGroup, String emergencyContact, boolean insured, String[] medicalRecordIds, double outstandingBalance, String registrationDate) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, nationalId, age, active);
+        this.allergies = allergies;
+        this.bloodGroup = bloodGroup;
+        this.emergencyContact = emergencyContact;
+        this.insured = insured;
+        this.medicalRecordIds = medicalRecordIds;
+        this.outstandingBalance = outstandingBalance;
+        this.registrationDate = registrationDate;
+    }
 }
