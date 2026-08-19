98b7f7632b10022aee5e34dee267587311045e68
mohammed <mooodo150@gmail.com>
2026-08-19 16:05:03 +0400

    feat(entities):I add full constructor [Task 1.5]

--- src/entities/MedicalRecord.java
+++ src/entities/MedicalRecord.java
@@ -10,4 +10,14 @@ public class MedicalRecord {
     private String notes;
     private boolean confidential;
 
+    public MedicalRecord(boolean confidential, String diagnosis, String doctorId, String notes, String patientId, String prescription, String recordId, String visitDate) {
+        this.confidential = confidential;
+        this.diagnosis = diagnosis;
+        this.doctorId = doctorId;
+        this.notes = notes;
+        this.patientId = patientId;
+        this.prescription = prescription;
+        this.recordId = recordId;
+        this.visitDate = visitDate;
+    }
 }
