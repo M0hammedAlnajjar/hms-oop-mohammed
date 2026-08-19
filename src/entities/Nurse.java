12ed81c89f899bef9fd8cf6c4ad6950ce182a312
mohammed <mooodo150@gmail.com>
2026-08-19 14:34:02 +0400

    feat(entities): add Nures attributes[Task 1:4]

--- src/entities/Nurse.java
+++ src/entities/Nurse.java
@@ -1,4 +1,9 @@
 package entities;
 
 public class Nurse extends Person{
+    private String departmentId;
+    private String shift;
+    private String[] assignedPatientIds;
+    private int yearsOfService;
+
 }
