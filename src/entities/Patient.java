034af00a6b033e8378119f39c8294315afacf92f
mohammed <mooodo150@gmail.com>
2026-08-19 10:28:21 +0400

    feat(entities):I use getters and setters [Task 1.2]

--- src/entities/Patient.java
+++ src/entities/Patient.java
@@ -20,6 +20,62 @@ public class Patient extends Person{
         this.registrationDate = registrationDate;
     }
 
+    public String[] getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String[] allergies) {
+        this.allergies = allergies;
+    }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public String getEmergencyContact() {
+        return emergencyContact;
+    }
+
+    public void setEmergencyContact(String emergencyContact) {
+        this.emergencyContact = emergencyContact;
+    }
+
+    public boolean isInsured() {
+        return insured;
+    }
+
+    public void setInsured(boolean insured) {
+        this.insured = insured;
+    }
+
+    public String[] getMedicalRecordIds() {
+        return medicalRecordIds;
+    }
+
+    public void setMedicalRecordIds(String[] medicalRecordIds) {
+        this.medicalRecordIds = medicalRecordIds;
+    }
+
+    public double getOutstandingBalance() {
+        return outstandingBalance;
+    }
+
+    public void setOutstandingBalance(double outstandingBalance) {
+        this.outstandingBalance = outstandingBalance;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
     @Override
     public void displayInfo() {
         super.displayInfo();
