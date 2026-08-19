b0940ef2d878805209721c9489e4a210e8978255
mohammed <mooodo150@gmail.com>
2026-08-19 08:53:44 +0400

    feat(entities): Person full constructo

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -12,4 +12,19 @@ public class Person {
     private String nationalId;
     private int age;
     private boolean active;
+
+    public Person(boolean active, String address, int age, String dateOfBirth, String email, String firstName, String gender, String id, String lastName, String nationalId, String phoneNumber) {
+        this.active = active;
+        this.address = address;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+        this.firstName = firstName;
+        this.gender = gender;
+        this.id = id;
+        this.lastName = lastName;
+        this.nationalId = nationalId;
+        this.phoneNumber = phoneNumber;
+    }
 }
+
