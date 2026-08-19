82a4f3f45a9645a8f7c78e06cb0abaf57947c8d3
mohammed <mooodo150@gmail.com>
2026-08-19 08:50:43 +0400

    feat(entities): add Person attributes

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -1,4 +1,15 @@
 package entities;
 
 public class Person {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private String nationalId;
+    private int age;
+    private boolean active;
 }
