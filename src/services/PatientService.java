dd02a635df9ff42338ffec482b6014b3868fd103
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-19 19:32:01 +0400

    feat(overload): findPatient + schedule overloads [Task 2.2]

---
+++ src/services/PatientService.java
@@ -0,0 +1,19 @@
+package services;
+
+
+import entities.Patient;
+
+public class PatientService {
+
+
+    // Find a patient by ID
+    public Patient findPatient(String id) {
+        return null;
+    }
+
+    // Find a patient by first name and last name
+    public Patient findPatient(String firstName, String lastName) {
+        return null;
+    }
+
+}
