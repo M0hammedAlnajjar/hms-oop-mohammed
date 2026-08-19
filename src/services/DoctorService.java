6dfd58323045e7d44fae6fa71bc6b8ccd59fa3e5
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-19 20:15:26 +0400

    feat(services): implement Manageable + Searchable in DoctorService [Task 2.4]

--- src/services/DoctorService.java
+++ src/services/DoctorService.java
@@ -1,4 +1,30 @@
 package services;
+import interfaces.Manageable;
+import interfaces.Searchable;
 
-public class DoctorService {
+public class DoctorService implements Manageable, Searchable {
+    @Override
+    public void add(Object entity) {
+
+    }
+
+    @Override
+    public void removeById(String id) {
+
+    }
+
+    @Override
+    public Object[] getAll() {
+        return new Object[0];
+    }
+
+    @Override
+    public Object[] search(String keyword) {
+        return new Object[0];
+    }
+
+    @Override
+    public Object searchById(String id) {
+        return null;
+    }
 }
