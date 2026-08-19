300f98505461e35424c40bb4eda9daa2e22fac08
mohammed <mooodo150@gmail.com>
2026-08-19 14:40:39 +0400

    feat(entities): I create getter and setter [Task 1.4]

--- src/entities/Nurse.java
+++ src/entities/Nurse.java
@@ -7,26 +7,59 @@ public class Nurse extends Person{
     private int yearsOfService;
 
 
-    public Nurse( String id,
-                  String firstName,
-                  String lastName,
-                  String dateOfBirth,
-                  String gender,
-                  String phoneNumber,
-                  String email,
-                  String address,
-                  String nationalId,
-                  int age,
-                  boolean active,
-                  String departmentId,
-                  String shift,
-                  String[] assignedPatientIds,
-                  int yearsOfService) {
+    public String[] getAssignedPatientIds() {
+        return assignedPatientIds;
+    }
+
+    public void setAssignedPatientIds(String[] assignedPatientIds) {
+        this.assignedPatientIds = assignedPatientIds;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+
+    public int getYearsOfService() {
+        return yearsOfService;
+    }
+
+    public void setYearsOfService(int yearsOfService) {
+        this.yearsOfService = yearsOfService;
+    }
+
+    public Nurse(String id,
+                 String firstName,
+                 String lastName,
+                 String dateOfBirth,
+                 String gender,
+                 String phoneNumber,
+                 String email,
+                 String address,
+                 String nationalId,
+                 int age,
+                 boolean active,
+                 String departmentId,
+                 String shift,
+                 String[] assignedPatientIds,
+                 int yearsOfService) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, nationalId, age, active);
         this.departmentId = departmentId;
         this.shift = shift;
         this.assignedPatientIds = assignedPatientIds;
         this.yearsOfService = yearsOfService;
+
 
     }
 }
