57c74d7b1687e0a1f03fc6675f4c27f611ab7be0
mohammed <mooodo150@gmail.com>
2026-08-19 16:12:41 +0400

    feat(entities): Appointment class + fields   [Task 1.6]

--- src/entities/Appointment.java
+++ src/entities/Appointment.java
@@ -1,4 +1,23 @@
 package entities;
 
 public class Appointment {
+    private String appointmentId;
+    private String patientId;
+    private String doctorId;
+    private String appointmentDate;
+    private String appointmentTime;
+    private String status;
+    private String reason;
+    private boolean followUp;
+
+    public Appointment(String appointmentDate, String appointmentId, String appointmentTime, String doctorId, boolean followUp, String patientId, String reason, String status) {
+        this.appointmentDate = appointmentDate;
+        this.appointmentId = appointmentId;
+        this.appointmentTime = appointmentTime;
+        this.doctorId = doctorId;
+        this.followUp = followUp;
+        this.patientId = patientId;
+        this.reason = reason;
+        this.status = status;
+    }
 }
