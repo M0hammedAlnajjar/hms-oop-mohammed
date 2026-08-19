c38a08c63fca9fa730b5ab3cac77b9c2e2c9516f
mohammed <mooodo150@gmail.com>
2026-08-19 16:03:45 +0400

    feat(entities):I add attributes [Task 1.5]

--- src/entities/MedicalRecord.java
+++ src/entities/MedicalRecord.java
@@ -1,5 +1,13 @@
 package entities;
 
 public class MedicalRecord {
+    private String recordId;
+    private String patientId;
+    private String doctorId;
+    private String visitDate;
+    private String diagnosis;
+    private String prescription;
+    private String notes;
+    private boolean confidential;
 
 }
