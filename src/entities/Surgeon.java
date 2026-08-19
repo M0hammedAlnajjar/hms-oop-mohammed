11c3e4e6bee7538edd8a9955d612d0bd579ac6c4
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-19 18:09:51 +0400

    feat(entities): Surgeon extends Doctor + fields  [Task 2.1]

--- src/entities/Surgeon.java
+++ src/entities/Surgeon.java
@@ -4,4 +4,53 @@ public class Surgeon extends Doctor  {
     private int surgeriesPerformed;
     private boolean operationTheatreAccess;
     private String[] upcomingSurgeryDates;
+
+    public Surgeon(
+            String id,
+            String firstName,
+            String lastName,
+            String dateOfBirth,
+            String gender,
+            String phoneNumber,
+            String email,
+            String address,
+            String nationalId,
+            int age,
+            boolean active,
+            String[] assignedPatientIds,
+            String[] availableSlots,
+            double consultationFee,
+            int experienceYears,
+            boolean onCall,
+            String specialization,
+            int surgeriesPerformed,
+            boolean operationTheatreAccess,
+            String[] upcomingSurgeryDates
+    ) {
+
+
+        super(
+                id,
+                firstName,
+                lastName,
+                dateOfBirth,
+                gender,
+                phoneNumber,
+                email,
+                address,
+                nationalId,
+                age,
+                active,
+                assignedPatientIds,
+                availableSlots,
+                consultationFee,
+                experienceYears,
+                onCall,
+                specialization
+        );
+
+        this.surgeriesPerformed = surgeriesPerformed;
+        this.operationTheatreAccess = operationTheatreAccess;
+        this.upcomingSurgeryDates = upcomingSurgeryDates;
+    }
 }
