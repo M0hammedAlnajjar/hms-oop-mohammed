43161d4edf27e9c72b95640f4b23673b645f5751
Mohammed Alnajjar <mooodo150@gmail.com>
2026-08-19 23:12:00 +0400

    feat(services): implement listByShift [Task 2.7]

--- src/services/NurseService.java
+++ src/services/NurseService.java
@@ -79,6 +79,28 @@ public class NurseService implements Manageable, Searchable {
 
         return null;
     }
+    // List nurses by shift
+    public Nurse[] listByShift(String shift) {
+
+        Nurse[] results = new Nurse[nurseCount];
+        int resultCount = 0;
+
+        for (int i = 0; i < nurseCount; i++) {
+
+            if (nurses[i].getShift().equalsIgnoreCase(shift)) {
+                results[resultCount] = nurses[i];
+                resultCount++;
+            }
+        }
+
+        Nurse[] finalResults = new Nurse[resultCount];
+
+        for (int i = 0; i < resultCount; i++) {
+            finalResults[i] = results[i];
+        }
+
+        return finalResults;
+    }
     // Store nurses without generics
     private Nurse[] nurses = new Nurse[10];
     private int nurseCount = 0;
