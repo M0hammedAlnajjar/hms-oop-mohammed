3d2b3e8ecb00224f5fbef970e3b0a2869371154d
mohammed <mooodo150@gmail.com>
2026-08-20 11:19:34 +0400

    fix(inpatient):Add getters and setter and field and remove duplicate constructor and add validation [Task 2.1]

--- src/entities/InPatient.java
+++ src/entities/InPatient.java
@@ -1,13 +1,32 @@
 package entities;
 
-public class InPatient extends Patient{
+public class InPatient extends Patient {
+
     private String admissionDate;
     private String roomNumber;
     private double dailyCharges;
     private int daysAdmitted;
 
+    // Basic constructor
+    public InPatient(
+            String id,
+            String firstName,
+            String lastName,
+            String admissionDate,
+            double dailyCharges,
+            int daysAdmitted,
+            String roomNumber
+    ) {
 
+        super(id, firstName, lastName);
 
+        setAdmissionDate(admissionDate);
+        setDailyCharges(dailyCharges);
+        setDaysAdmitted(daysAdmitted);
+        setRoomNumber(roomNumber);
+    }
+
+    // Full constructor
     public InPatient(
             String id,
             String firstName,
@@ -55,16 +74,59 @@ public class InPatient extends Patient{
                 registrationDate
         );
 
-        this.admissionDate = admissionDate;
-        this.roomNumber = roomNumber;
-        this.dailyCharges = dailyCharges;
-        this.daysAdmitted = daysAdmitted;
+        setAdmissionDate(admissionDate);
+        setRoomNumber(roomNumber);
+        setDailyCharges(dailyCharges);
+        setDaysAdmitted(daysAdmitted);
     }
 
+    public String getAdmissionDate() {
+        return admissionDate;
+    }
 
+    public void setAdmissionDate(String admissionDate) {
+
+        if (admissionDate != null && !admissionDate.isBlank()) {
+            this.admissionDate = admissionDate;
+        }
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+
+        if (roomNumber != null && !roomNumber.isBlank()) {
+            this.roomNumber = roomNumber;
+        }
+    }
+
+    public double getDailyCharges() {
+        return dailyCharges;
+    }
+
+    public void setDailyCharges(double dailyCharges) {
+
+        if (dailyCharges >= 0) {
+            this.dailyCharges = dailyCharges;
+        }
+    }
+
+    public int getDaysAdmitted() {
+        return daysAdmitted;
+    }
+
+    public void setDaysAdmitted(int daysAdmitted) {
+
+        if (daysAdmitted >= 0) {
+            this.daysAdmitted = daysAdmitted;
+        }
+    }
 
     @Override
     public void displayInfo() {
+
         super.displayInfo();
 
         System.out.println("Admission Date: " + admissionDate);
@@ -72,23 +134,28 @@ public class InPatient extends Patient{
         System.out.println("Daily Charges: " + dailyCharges);
         System.out.println("Days Admitted: " + daysAdmitted);
     }
+
     public void admit(
             String admissionDate,
             String roomNumber,
             double dailyCharges,
             int daysAdmitted
     ) {
-        this.admissionDate = admissionDate;
-        this.roomNumber = roomNumber;
-        this.dailyCharges = dailyCharges;
-        this.daysAdmitted = daysAdmitted;
+
+        setAdmissionDate(admissionDate);
+        setRoomNumber(roomNumber);
+        setDailyCharges(dailyCharges);
+        setDaysAdmitted(daysAdmitted);
     }
+
     public void discharge() {
+
         admissionDate = null;
         roomNumber = null;
         dailyCharges = 0;
         daysAdmitted = 0;
     }
+
     public double totalRoomCost() {
         return dailyCharges * daysAdmitted;
     }
