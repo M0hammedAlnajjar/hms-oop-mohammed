775e5f6bf0f25a9e2bd703ea7c49d5987efb7029
mohammed <mooodo150@gmail.com>
2026-08-20 15:04:08 +0400

    fix(inpatient): remove extra closing brace [Task 2.1]

--- src/entities/InPatient.java
+++ src/entities/InPatient.java
@@ -1,11 +1,11 @@
 package entities;
 
-public class InPatient extends Patient{
+public class InPatient extends Patient {
+
     private String admissionDate;
     private String roomNumber;
     private double dailyCharges;
     private int daysAdmitted;
-
 
 
     public InPatient(
@@ -62,34 +62,54 @@ public class InPatient extends Patient{
     }
 
 
-    }
     @Override
     public void displayInfo() {
+
         super.displayInfo();
 
-        System.out.println("Admission Date: " + admissionDate);
-        System.out.println("Room Number: " + roomNumber);
-        System.out.println("Daily Charges: " + dailyCharges);
-        System.out.println("Days Admitted: " + daysAdmitted);
+        System.out.println(
+                "Admission Date: " + admissionDate
+        );
+
+        System.out.println(
+                "Room Number: " + roomNumber
+        );
+
+        System.out.println(
+                "Daily Charges: " + dailyCharges
+        );
+
+        System.out.println(
+                "Days Admitted: " + daysAdmitted
+        );
     }
+
+
     public void admit(
             String admissionDate,
             String roomNumber,
             double dailyCharges,
             int daysAdmitted
     ) {
+
         this.admissionDate = admissionDate;
         this.roomNumber = roomNumber;
         this.dailyCharges = dailyCharges;
         this.daysAdmitted = daysAdmitted;
     }
+
+
     public void discharge() {
+
         admissionDate = null;
         roomNumber = null;
         dailyCharges = 0;
         daysAdmitted = 0;
     }
+
+
     public double totalRoomCost() {
+
         return dailyCharges * daysAdmitted;
     }
 }
